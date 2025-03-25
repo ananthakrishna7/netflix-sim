169d43128f1ded5c1c5f92cd011c796911e928fd
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2025-03-25 12:21:33 +0000

    Add function to split video into chunks

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,11 @@
 module github.com/ananthakrishna7/netflix-sim.git/server
 
 go 1.22.5
+
+require (
+	github.com/aws/aws-sdk-go v1.55.6 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
+	github.com/zencoder/go-dash v0.0.0-20201006100653-2f93b14912b2 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -20,6 +22,19 @@ func headers(w http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	fmt.Println(os.Getwd())
+	video_split("video1.mp4", "videos")
+	// http.ListenAndServe(":8090", nil)
+}
 
-	http.ListenAndServe(":8090", nil)
+// pass this command to os.exec.Command(). That's it.
+// ffmpeg -i video1.mp4 -c copy -map 0 -segment_time 00:00:10 -f segment -reset_timestamps 1 output%03d.mp4D
+func video_split(inPath string, outPath string) {
+	cmd := exec.Command("ffmpeg", "-i", inPath, "-c", "copy", "-map", "0", "-segment_time", "00:00:30", "-f", "segment", "-reset_timestamps", "1", outPath+"/output%03d.mp4")
+	_, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Print(output)
+	// assert.Nil(t, err)
 }
