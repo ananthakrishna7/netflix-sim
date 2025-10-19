b9dc87e551032398e9b38f481fc90b7afc3645a3
Ananthakrishnan Balajee <mozhenboo@gmail.com>
2025-10-19 22:52:08 +0530

    Enhance file upload functionality and improve index page layout

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -18,11 +20,6 @@ func headers(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
 	}
-}
-
-func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "You have reached the index page. I haven't done anything here. Go to /hello or /headers :P\n")
-	// http.
 }
 
 func uploadFile(w http.ResponseWriter, req *http.Request) {
@@ -40,44 +37,61 @@ func uploadFile(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// next step, make chunks and make those persistent. can discard original. i think. maybe. :O
+	dst, err := os.Create("videos/" + handler.Filename)
+	if err != nil {
+		log.Println("error creating file", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "uploaded file")
+	http.Redirect(w, req, "/index", http.StatusSeeOther)
+}
 
-	tempFile, err := os.CreateTemp("temp-files", "upload-*.mp4") // should change the format to accept more filetypes
+func videoSplit() {
+	files, err := os.ReadDir("videos/")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer tempFile.Close()
-
-	fileBytes, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
+	for _, v := range files {
+		inPath := v.Name()
+		outPath := strings.Split(v.Name(), ".")[0] + "/"
+		cmd := exec.Command("ffmpeg", "-i", inPath, "-c", "copy", "-map", "0", "-segment_time", "00:00:30", "-f", "segment", "-reset_timestamps", "1", outPath+"/segment%03d.mp4")
+		out, err := cmd.Output()
+		fmt.Println(out)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
-	tempFile.Write(fileBytes)
-
-	fmt.Fprintf(w, "Successfully uploaded File\n")
-
+	fmt.Println("Files split")
+	fmt.Println(os.ReadDir("./videos"))
 }
 
 func main() {
-
+	go videoSplit()
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/upload", uploadFile)
+	// http.HandleFunc("/process", videoSplit)
 	http.ListenAndServe(":8090", nil)
+	entries, err := os.ReadDir("videos/")
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	fmt.Printf("Files in /videos:\n")
+	for _, entry := range entries {
+		fmt.Println(entry.Name())
+	}
 }
 
 // pass this command to os.exec.Command(). That's it.
 // ffmpeg -i video1.mp4 -c copy -map 0 -segment_time 00:00:10 -f segment -reset_timestamps 1 output%03d.mp4D
-func video_split(inPath string, outPath string) {
-	cmd := exec.Command("ffmpeg", "-i", inPath, "-c", "copy", "-map", "0", "-segment_time", "00:00:30", "-f", "segment", "-reset_timestamps", "1", outPath+"/output%03d.mp4")
-	_, err := cmd.Output()
-	if err != nil {
-		panic(err)
-	}
-	// fmt.Print(output)
-	// assert.Nil(t, err)
-}
